02248e5eeb11743965381a46a057df14114b78eb
DexusJose <jose_perezmiranda@outlook.com>
2018-10-25 17:43:03 -0500

    Se agrego el proyecto BaseDatos.

---
+++ BaseDatos/src/forms/ventanaPrincipal.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package forms;
+
+import BaseDatos.*;
+import basedatos.BaseDatos;
+import com.sun.prism.Image;
+import java.awt.Color;
+import static java.awt.Frame.MAXIMIZED_BOTH;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+/**
+ *
+ * @author Almacen
+ */
+public class ventanaPrincipal {
+    JFrame ventana;
+    public Color fondoBarra = new Color(51, 204, 204);
+    public Color fondoVentana = new Color(102, 179, 255);
+    public JMenuBar menuBar;
+    public JMenu m_Archivo;
+    public JMenuItem m_salir,add_reg;
+    
+    public final ImageIcon icono = new ImageIcon(BaseDatos.class.getResource("/recursos/icono/AUV.png"));
+    public  void crearventana(){
+        configurarVentana();
+    }
+
+    private void configurarVentana()  {
+        ventana = new JFrame();
+        menuBar = new JMenuBar();
+        
+        // declaracion de menu
+        m_Archivo = new JMenu("Archivo"); 
+        m_salir = new JMenuItem("Salir");
+        add_reg = new JMenuItem("Agregar Registro");
+        
+        //agregado deitems del menu
+        m_Archivo.add(add_reg);
+        
+        
+        m_Archivo.add(m_salir);
+        
+        menuBar.add(m_Archivo);
+        menuBar.setBackground(fondoBarra);
+        
+        
+        
+        m_salir.addActionListener(new ActionListener(){public void actionPerformed(ActionEvent e){System.exit(EXIT_ON_CLOSE);}});
+        
+        
+        ventana.setTitle("Administracion de Base de Datos");
+        
+        ventana.setSize(MAXIMIZED_BOTH, MAXIMIZED_BOTH);
+        ventana.setExtendedState(MAXIMIZED_BOTH);
+        ventana.getContentPane().setBackground(fondoVentana);
+        ventana.setIconImage(icono.getImage());
+        ventana.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ventana.pack();
+        ventana.setVisible(true);
+        ventana.setJMenuBar(menuBar);
+        
+        
+        
+    }
+    
+    
+}

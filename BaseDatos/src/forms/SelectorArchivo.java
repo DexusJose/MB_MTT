05c1cabee24c8a201691b685d6002dc423c4c11f
DexusJose <jose_perezmiranda@outlook.com>
2018-11-02 13:45:26 -0600

    Agregado el mini visor de texto y arreglado los problemas al cerrar la aplicacion y los directorios.

--- BaseDatos/src/forms/SelectorArchivo.java
+++ BaseDatos/src/forms/SelectorArchivo.java
@@ -5,6 +5,7 @@
  */
 package forms;
 
+import basedatos.BaseDatos;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
@@ -12,40 +13,92 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 /**
  *
  * @author Almacen
  */
 public class SelectorArchivo extends javax.swing.JFrame {
+    public final ImageIcon icono = new ImageIcon(BaseDatos.class.getResource("/recursos/icono/AUV.png"));
+    private final String RutaPorDefecto = "C:\\Users\\\\Desktop";
+    
+    JFileChooser selectDialog ;
+    
+    JFrame textWin;
+    File f;
+    FileInputStream fis;
+    String ruta;
     
     public void archivoSelector() throws FileNotFoundException, IOException{
         
-        JFrame textWin = new JFrame();
+        
+        //String ruta = "C:\\Users\\Almacen\\Desktop\\n.txt";
+        //String ruta = "C:\\Users\\sangr\\Desktop\\n.txt";
+        
+        
+        selectDialog = new JFileChooser(RutaPorDefecto);
+        //selectDialog.showOpenDialog(this);
+        selectDialog.setFileFilter(new FileNameExtensionFilter("Archivo de texto *.txt", "txt"));
+        
+        
+        selectDialog.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        
+        int resultado;
+        resultado = selectDialog.showOpenDialog(this);
+        this.dispose();
+        
+        switch(resultado) {
+            case JFileChooser.APPROVE_OPTION:
+            //seleccionó abrir
+                crearEditor();
+            break;
+
+            case JFileChooser.CANCEL_OPTION:
+            //dio click en cancelar o cerro la ventana
+                dispose();
+             break;
+
+            case JFileChooser.ERROR_OPTION:
+            //llega aqui si sucede un error
+            break;
+        }
+
+
+    }
+    
+    public void crearEditor() throws FileNotFoundException, IOException{
+        File archivo = selectDialog.getSelectedFile(); // obtiene el archivo seleccionado
+        ruta =  archivo.toString();
+        System.out.println(archivo);
+        // muestra error si es inválido
+        if ((archivo == null) || (archivo.getName().equals(""))) {
+        JOptionPane.showMessageDialog(this, "Nombre de archivo inválido", "Nombre de archivo inválido", JOptionPane.ERROR_MESSAGE);
+        } // fin de if
+        
+        
+        textWin = new JFrame();
         textWin.setTitle("Texto");
         textWin.setSize(800,600);
         textWin.setExtendedState(NORMAL);
+        textWin.setIconImage(icono.getImage());
         textWin.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        //textWin.setLocationRelativeTo(null);
+        textWin.setLocationRelativeTo(null);
+        textWin.setPreferredSize(new Dimension(800,600));
         //textWin.setResizable(false);
         //textWin.setLayout(null);
         //textWin.pack();
         textWin.setVisible(true);
-        
-//        JScrollPane scroll = new JScrollPane();
-//        textWin.add(scroll,BorderLayout.CENTER);
-        
-        
-        
-        //String ruta = "C:\\Users\\Almacen\\Desktop\\n.txt";
-        String ruta = "C:\\Users\\sangr\\Desktop\\n.txt";
-        File f = new File(ruta);
+      
+        f = new File(ruta);
         System.out.println(f);
-        FileInputStream fis = new FileInputStream(f);
+        fis= new FileInputStream(f);
         byte[] c = new byte[fis.available()];
         System.out.println(c);
         fis.read(c);
@@ -63,15 +116,5 @@ public class SelectorArchivo extends javax.swing.JFrame {
         JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         areaScrollPane.setPreferredSize(new Dimension(250, 250));
         textWin.add(areaScrollPane);
-        //areaScrollPane.setVisible(true);
-        
-//        JFileChooser selectDialog ;
-//        selectDialog = new JFileChooser();
-//        
-//        selectDialog.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-//        
-//        int resultado;
-//        resultado = selectDialog.showOpenDialog(this);
-//        this.dispose();
     }
 }
